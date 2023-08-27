8eb38f0f8e46e21d7cc497043323d4865dc87651
miikatu <miikatti@gmail.com>
2023-08-27 21:46:32 +0300

    code cleanup

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
     <div className="p-10 ">
       <div className="bg-black relative rounded ">
         <div id="box-violet" className="grid grid-cols-4 rounded place-items-center -translate-x-4 -translate-y-4 bg-violet-600">
-          <img id="logo" className="col-start-1  m-2 pl-2 grid-start-1 col-span-1" src="/assets/logo.svg" alt="Logo" />
+          <img id="logo" className=" col-start-1  m-2 pl-2 grid-start-1 col-span-1" src="/assets/logo.svg" alt="Logo" />
           <div id="navbar-buttons" className="col-start-3 sm:hidden shrink-0 flex ">
             <img className="h-14 w-14 bg-rounded hover:bg-white bg-clip-border border-8 border-transparent rounded-2xl" src="/assets/home.svg" alt="Logo" />
             <img className="h-12 w-12 " src="/assets/hamburger.svg" alt="Logo" />
